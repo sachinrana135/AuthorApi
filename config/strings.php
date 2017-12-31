a555f5f8431d9fcab0087e5672e88b72514af818
Sachin Rana <sachinrana135@gmail.com>
2017-12-31 20:34:24 +0530

    Updates texts in strings

--- config/strings.php
+++ config/strings.php
@@ -8,10 +8,16 @@ return [
    |--------------------------------------------------------------------------
    */
 
-    'push_message_new_like' => 'Congrats, You have got one new like. Click to see.',
-    'push_message_new_comment' => 'Congrats, You have got one new comment. Click to see.',
-    'push_message_quote_report' => 'Someone has reported your quote against policy violation. We will review your quote but till then, your quote will not be active.',
-    'push_message_comment_report' => 'Someone has reported your comment against policy violation. We will review your quote but till then, your comment will not be active.',
+    'push_message_new_like_title' => '+1 like',
+    'push_message_new_comment_title' => '+1 comment..',
+    'push_message_quote_report_title' => 'Policy violation',
+    'push_message_comment_report_title' => 'Policy violation',
+    
+   
+    'push_message_new_like_message' => 'Congratulations, You have got one new like. Click to see.',    
+    'push_message_new_comment_message' => 'Congratulations, You have got one new comment. Click to see.',
+    'push_message_quote_report_message' => 'Someone has reported your quote against policy violation. We will review your quote but till then, your quote will be in-active.',
+    'push_message_comment_report_message' => 'Someone has reported your comment against policy violation. We will review your comment but till then, your comment will be in-active.',
   
 
 ];
